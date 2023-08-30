0246ff8b66d5c7fdfc04dd7a6cfdb1b4ab62cd9b
Eltik <xp10d363@gmail.com>
2023-08-30 19:15:49 -0400

    Fix subtitles for Anify

--- Video/Anify/dist/Media/code.js
+++ Video/Anify/dist/Media/code.js
@@ -13,35 +13,40 @@ async function logic(payload) {
 }
 async function getSource(payload) {
     const data = JSON.parse(await sendRequest(payload.query, {}));
-    sendResult({
-        result: {
-            skips: [
-                {
-                    start: data.intro.start,
-                    end: data.intro.end,
-                    type: "Opening",
-                },
-                {
-                    start: data.outro.start,
-                    end: data.outro.end,
-                    type: "Ending",
-                },
-            ],
-            sources: data.sources.map((source) => {
-                return {
-                    file: source.url,
-                    type: "hls",
-                    quality: source.quality,
-                };
-            }),
-            subtitles: data.subtitles.map((subtitle) => {
-                return {
-                    url: subtitle.url,
-                    language: subtitle.lang,
-                };
-            }),
-            headers: data.headers,
-        },
-        action: "video",
-    }, true);
+    const subtitles = [];
+    data.subtitles.map((subtitle) => {
+        if (subtitle.lang.toLowerCase() === "thumbnails") {
+            return [];
+        }
+        subtitles.push({
+            url: subtitle.url,
+            language: subtitle.lang,
+        });
+    }),
+        sendResult({
+            result: {
+                skips: [
+                    {
+                        start: data.intro.start,
+                        end: data.intro.end,
+                        type: "Opening",
+                    },
+                    {
+                        start: data.outro.start,
+                        end: data.outro.end,
+                        type: "Ending",
+                    },
+                ],
+                sources: data.sources.map((source) => {
+                    return {
+                        file: source.url,
+                        type: "hls",
+                        quality: source.quality,
+                    };
+                }),
+                subtitles: subtitles,
+                headers: data.headers,
+            },
+            action: "video",
+        }, true);
 }
--- Video/Anify/src/Media/code.ts
+++ Video/Anify/src/Media/code.ts
@@ -17,38 +17,44 @@ async function logic(payload: BasePayload) {
 async function getSource(payload: any) {
     const data = JSON.parse(await sendRequest(payload.query, {}));
 
-    sendResult(
-        {
-            result: {
-                skips: [
-                    {
-                        start: data.intro.start,
-                        end: data.intro.end,
-                        type: "Opening",
-                    },
-                    {
-                        start: data.outro.start,
-                        end: data.outro.end,
-                        type: "Ending",
-                    },
-                ],
-                sources: data.sources.map((source: any) => {
-                    return {
-                        file: source.url,
-                        type: "hls",
-                        quality: source.quality,
-                    };
-                }),
-                subtitles: data.subtitles.map((subtitle: any) => {
-                    return {
-                        url: subtitle.url,
-                        language: subtitle.lang,
-                    };
-                }),
-                headers: data.headers,
+    const subtitles: { url: string; language: string }[] = [];
+
+    data.subtitles.map((subtitle: any) => {
+        if (subtitle.lang.toLowerCase() === "thumbnails") {
+            return [];
+        }
+        subtitles.push({
+            url: subtitle.url,
+            language: subtitle.lang,
+        });
+    }),
+        sendResult(
+            {
+                result: {
+                    skips: [
+                        {
+                            start: data.intro.start,
+                            end: data.intro.end,
+                            type: "Opening",
+                        },
+                        {
+                            start: data.outro.start,
+                            end: data.outro.end,
+                            type: "Ending",
+                        },
+                    ],
+                    sources: data.sources.map((source: any) => {
+                        return {
+                            file: source.url,
+                            type: "hls",
+                            quality: source.quality,
+                        };
+                    }),
+                    subtitles: subtitles,
+                    headers: data.headers,
+                },
+                action: "video",
             },
-            action: "video",
-        },
-        true
-    );
+            true
+        );
 }
